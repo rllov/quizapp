124dfa42a23ab04ebf3674b43c66e9fa85f21f6e
Ryan Lov <r.lee.lov@gmail.com>
2023-09-14 01:20:05 -0700

    Updated QuestionBank

--- src/Helpers/QuestionBank.js
+++ src/Helpers/QuestionBank.js
@@ -25,110 +25,110 @@ export const Questions = [
     optionD: "Null Case",
     answer: "D",
   },
-  //   {
-  //     prompt: "The Average case occurs in the linear search algorithm.",
-  //     optionA: "When Item is somewhere in the middle of the array",
-  //     optionB: "When Item is not in the array at all",
-  //     optionC: "When Item is the last element in the array",
-  //     optionD:
-  //       "When Item is the last element in the array or is not there at all",
-  //     answer: "A",
-  //   },
-  //   {
-  //     prompt: "The complexity of the linear search algorithm is",
-  //     optionA: "O(n)",
-  //     optionB: "O(log n)",
-  //     optionC: "O(n^2)",
-  //     optionD: "O(n log n)",
-  //     answer: "A",
-  //   },
-  //   {
-  //     prompt: "The complexity of the Binary search algorithm is",
-  //     optionA: "O(n)",
-  //     optionB: "O(log)",
-  //     optionC: "O(n^2)",
-  //     optionD: "O(n log n) ",
-  //     answer: "B",
-  //   },
-  //   {
-  //     prompt: "The complexity of the Bubble sort algorithm is",
-  //     optionA: "O(n)",
-  //     optionB: "O(log)",
-  //     optionC: "O(n^2)",
-  //     optionD: "O(n log n) ",
-  //     answer: "C",
-  //   },
-  //   {
-  //     prompt: "The complexity of the merge sort algorithm is",
-  //     optionA: "O(n)",
-  //     optionB: "O(log)",
-  //     optionC: "O(n^2)",
-  //     optionD: "O(n log n) ",
-  //     answer: "D",
-  //   },
-  //   {
-  //     prompt:
-  //       "Can you guess which of the following data structure is NOT a linear data structure?",
-  //     optionA: "Arrays",
-  //     optionB: "Linked List",
-  //     optionC: "Both of the Above",
-  //     optionD: "None of the Above",
-  //     answer: "D",
-  //   },
-  //   {
-  //     prompt:
-  //       "Can you guess which of the following data structure is linear data structure?",
-  //     optionA: "Trees",
-  //     optionB: "Graphs",
-  //     optionC: "Arrays",
-  //     optionD: "None of the Above",
-  //     answer: "C",
-  //   },
-  //   {
-  //     prompt:
-  //       "Which function findings the location of the element with a given value is:",
-  //     optionA: "Traversal",
-  //     optionB: "Search",
-  //     optionC: "Sort",
-  //     optionD: "None of the Above",
-  //     answer: "B",
-  //   },
-  //   {
-  //     prompt: "Arrays are the best data structures",
-  //     optionA: "For relatively permanent collections of data",
-  //     optionB:
-  //       "For the size of the structure and the data in the structure are constantly changing",
-  //     optionC: "For both of the above situation",
-  //     optionD: "For none of the above situation",
-  //     answer: "A",
-  //   },
-  //   {
-  //     prompt: "Linked lists are the best suited",
-  //     optionA: "For relatively permanent collections of data",
-  //     optionB:
-  //       "For the size of the structure and the data in the structure are constantly changing",
-  //     optionC: "For both of the above situation",
-  //     optionD: "For none of the above situation",
-  //     answer: "B",
-  //   },
-  //   {
-  //     prompt:
-  //       "Each array declaration does not give, implicitly or explicitly, the information about which of the following?",
-  //     optionA: "The name of the array",
-  //     optionB: "The exact data type of array",
-  //     optionC: "The first data from the set that will be stored",
-  //     optionD: "Index set",
-  //     answer: "C",
-  //   },
-  //   {
-  //     prompt:
-  //       "Why are the elements of an array are stored successively memory cells?",
-  //     optionA:
-  //       "In this way, the computer can keep track only the address of the first element, and the addresses of other elements can be calculated",
-  //     optionB:
-  //       "The architecture of computer memory does not allow arrays to store other than serially",
-  //     optionC: "Both of above",
-  //     optionD: "None of above",
-  //     answer: "A",
-  //   },
+  {
+    prompt: "The Average case occurs in the linear search algorithm.",
+    optionA: "When Item is somewhere in the middle of the array",
+    optionB: "When Item is not in the array at all",
+    optionC: "When Item is the last element in the array",
+    optionD:
+      "When Item is the last element in the array or is not there at all",
+    answer: "A",
+  },
+  {
+    prompt: "The complexity of the linear search algorithm is",
+    optionA: "O(n)",
+    optionB: "O(log n)",
+    optionC: "O(n^2)",
+    optionD: "O(n log n)",
+    answer: "A",
+  },
+  {
+    prompt: "The complexity of the Binary search algorithm is",
+    optionA: "O(n)",
+    optionB: "O(log)",
+    optionC: "O(n^2)",
+    optionD: "O(n log n) ",
+    answer: "B",
+  },
+  {
+    prompt: "The complexity of the Bubble sort algorithm is",
+    optionA: "O(n)",
+    optionB: "O(log)",
+    optionC: "O(n^2)",
+    optionD: "O(n log n) ",
+    answer: "C",
+  },
+  {
+    prompt: "The complexity of the merge sort algorithm is",
+    optionA: "O(n)",
+    optionB: "O(log)",
+    optionC: "O(n^2)",
+    optionD: "O(n log n) ",
+    answer: "D",
+  },
+  {
+    prompt:
+      "Can you guess which of the following data structure is NOT a linear data structure?",
+    optionA: "Arrays",
+    optionB: "Linked List",
+    optionC: "Both of the Above",
+    optionD: "None of the Above",
+    answer: "D",
+  },
+  {
+    prompt:
+      "Can you guess which of the following data structure is linear data structure?",
+    optionA: "Trees",
+    optionB: "Graphs",
+    optionC: "Arrays",
+    optionD: "None of the Above",
+    answer: "C",
+  },
+  {
+    prompt:
+      "Which function findings the location of the element with a given value is:",
+    optionA: "Traversal",
+    optionB: "Search",
+    optionC: "Sort",
+    optionD: "None of the Above",
+    answer: "B",
+  },
+  {
+    prompt: "Arrays are the best data structures",
+    optionA: "For relatively permanent collections of data",
+    optionB:
+      "For the size of the structure and the data in the structure are constantly changing",
+    optionC: "For both of the above situation",
+    optionD: "For none of the above situation",
+    answer: "A",
+  },
+  {
+    prompt: "Linked lists are the best suited",
+    optionA: "For relatively permanent collections of data",
+    optionB:
+      "For the size of the structure and the data in the structure are constantly changing",
+    optionC: "For both of the above situation",
+    optionD: "For none of the above situation",
+    answer: "B",
+  },
+  {
+    prompt:
+      "Each array declaration does not give, implicitly or explicitly, the information about which of the following?",
+    optionA: "The name of the array",
+    optionB: "The exact data type of array",
+    optionC: "The first data from the set that will be stored",
+    optionD: "Index set",
+    answer: "C",
+  },
+  {
+    prompt:
+      "Why are the elements of an array are stored successively memory cells?",
+    optionA:
+      "In this way, the computer can keep track only the address of the first element, and the addresses of other elements can be calculated",
+    optionB:
+      "The architecture of computer memory does not allow arrays to store other than serially",
+    optionC: "Both of above",
+    optionD: "None of above",
+    answer: "A",
+  },
 ];
